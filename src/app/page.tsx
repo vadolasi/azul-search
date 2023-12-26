d1299827e0a340a7122fecaff4ded04bc45b2ee5
Vitor Daniel <vitor036daniel@gmail.com>
2023-12-26 11:27:23 -0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,248 @@
-import Image from 'next/image'
+"use client"
+
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+import { useRouter } from "next/navigation"
+import useSWR from "swr"
+
+const schema = z.object({
+  origem: z.object({
+    code: z.string(),
+    name: z.string()
+  }),
+  destino: z.object({
+    code: z.string(),
+    name: z.string()
+  }),
+  dataSaida: z.date().refine(value => {
+    const data = new Date(value)
+    const hoje = new Date()
+    hoje.setHours(0, 0, 0, 0)
+    return data >= hoje
+  }, "A data de saída deve ser maior ou igual a data atual"),
+  dataRetorno: z.date().refine(value => {
+    const data = new Date(value)
+    const hoje = new Date()
+    hoje.setHours(0, 0, 0, 0)
+    return data >= hoje
+  }, "A data de retorno deve ser maior ou igual a data atual").optional(),
+  adultos: z.number().int().min(1).max(9),
+  criancas: z.number().int().min(0).max(9),
+  bebes: z.number().int().min(0).max(9),
+  onlyDirect: z.boolean().optional(),
+  onlyOneWay: z.boolean().optional(),
+  cabinCategory: z.enum(["ECONOMY", "BUSINESS"]).default("ECONOMY")
+})
+  .refine(value => {
+    if (!value.dataRetorno) {
+      return true
+    }
+
+    const dataSaida = new Date(value.dataSaida)
+    const dataRetorno = new Date(value.dataRetorno)
+    return dataSaida <= dataRetorno
+  }, "A data de retorno deve ser maior ou igual a data de saída")
+
+type FormValues = z.infer<typeof schema>
 
 export default function Home() {
+  const [origensOpen, setOrigensOpen] = useState(false)
+  const [destinosOpen, setDestinosOpen] = useState(false)
+  const [lastDestino, setLastDestino] = useState<string | null>(null)
+  const [lastOrigem, setLastOrigem] = useState<string | null>(null)
+
+  const { register, handleSubmit, formState: { errors }, watch, setValue } = useForm<FormValues>({
+    resolver: zodResolver(schema),
+    mode: "onBlur",
+    defaultValues: {
+      origem: {
+        code: "",
+        name: ""
+      },
+      destino: {
+        code: "",
+        name: ""
+      },
+      adultos: 1,
+      criancas: 0,
+      bebes: 0,
+      dataSaida: new Date()
+    }
+  })
+
+  const router = useRouter()
+
+  const onSubmit = handleSubmit(data => {
+    const searchParams = new URLSearchParams()
+    searchParams.set("tripType", data.onlyOneWay ? "ONE_WAY" : "ROUND_TRIP")
+    searchParams.set("from", data.origem.code)
+    searchParams.set("to", data.destino.code)
+    searchParams.set("adult", data.adultos.toString())
+    searchParams.set("child", data.criancas.toString())
+    searchParams.set("infant", data.bebes.toString())
+    searchParams.set("cabin", data.cabinCategory)
+    searchParams.set("flightType", data.onlyDirect ? "DIRECT" : "ALL")
+    searchParams.set("companies", "-")
+    searchParams.set("derpatureDateTime", data.dataSaida.toISOString())
+    if (data.dataRetorno) {
+      searchParams.set("returnDateTime", data.dataRetorno.toISOString())
+    }
+
+    router.push(`/search?${searchParams.toString()}`)
+  })
+
+  const origem = watch("origem.name")
+  const origemCode = watch("origem.code")
+  const destino = watch("destino.name")
+  const dataSaida = watch("dataSaida")
+  const onlyOneWay = watch("onlyOneWay")
+
+  const { data: origens } = useSWR<{ code: string, name: string }[]>(
+    () => {
+      if (origem.length >= 3 && origem !== lastOrigem) {
+        return `https://interline.tudoazul.com/catalog/api/v1/airport?searchAirport=${origem}`
+      }
+
+      return null
+    },
+    url => fetch(url).then(res => res.json()),
+    {
+      onSuccess: () => {
+        setOrigensOpen(true)
+      }
+    }
+  )
+
+  const { data: destinos } = useSWR<{ code: string, name: string }[]>(
+    () => {
+      if (origemCode && destino.length >= 3 && destino !== lastDestino) {
+        return `https://interline.tudoazul.com/catalog/api/v1/airport?searchAirport=${destino}&originAirportCode=${origemCode}`
+      }
+
+      return null
+    },
+    url => fetch(url).then(res => res.json()),
+    {
+      onSuccess: () => {
+        setDestinosOpen(true)
+      }
+    }
+  )
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex gap-4 items-center justify-center w-full min-h-screen">
+      <form className="flex flex-col gap-8 w-1/2" onSubmit={onSubmit}>
+        <label htmlFor="input_onlyOneWay">
+          <input id="input_onlyOneWay" type="checkbox" className="mr-2" {...register("onlyOneWay")} />
+          Somente ida
+        </label>
+        <div className="flex gap-4">
+          <div className="relative w-full">
+            <label htmlFor="input_origem">Origem</label>
+            <input id="input_origem" type="text" className="p-2 border bg-white rounded w-full" {...register("origem.name")} />
+
+            {origensOpen && (
+              <ul className="absolute rounded border shadow mt-1 max-h-48 overflow-auto z-50 w-full bg-white">
+                {origens?.map(origem => (
+                  <li key={origem.code}>
+                    <button
+                      onClick={() => {
+                        setValue("origem.name", origem.name)
+                        setValue("origem.code", origem.code)
+                        setLastOrigem(origem.name)
+                        setOrigensOpen(false)
+                      }}
+                      className="p-1 rounded hover:bg-gray-100 w-full text-left text-sm"
+                    >
+                      {origem.name}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+          <div className="relative w-full">
+            <label htmlFor="input_destino">Destino</label>
+            <input id="input_destino" type="text" className="p-2 border bg-white rounded w-full" {...register("destino.name")} />
+
+            {destinosOpen && (
+              <ul className="absolute rounded border shadow mt-1 max-h-48 overflow-auto z-50 w-full bg-white">
+                {destinos?.map(destino => (
+                  <li key={destino.code}>
+                    <button
+                      onClick={() => {
+                        setValue("destino.name", destino.name)
+                        setValue("destino.code", destino.code)
+                        setLastDestino(destino.name)
+                        setDestinosOpen(false)
+                      }}
+                      className="p-1 rounded hover:bg-gray-100 w-full text-left text-sm"
+                    >
+                      {destino.name}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className="flex gap-4 w-full">
+          <div className="flex flex-col w-full">
+            <label htmlFor="input_data_saida">Data de saída</label>
+            <input
+              id="input_data_saida"
+              type="date"
+              className="p-2 border bg-white rounded"
+              {...register("dataSaida", { valueAsDate: true })} min={new Date(Date.now() + 4 * 24 * 60 * 60 * 1000).toISOString().split("T")[0]}
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            {!onlyOneWay && (
+              <>
+                <label htmlFor="input_data_retorno">Data de retorno</label>
+                <input
+                  id="input_data_retorno"
+                  type="date"
+                  className="p-2 border bg-white rounded disabled:opacity-50"
+                  {...register("dataRetorno", { valueAsDate: true })}
+                  disabled={!dataSaida}
+                  min={dataSaida ? dataSaida.toISOString().split("T")[0] : ""}
+                />
+              </>
+            )}
+          </div>
+        </div>
+        <div className="flex gap-4 w-full">
+          <div className="flex flex-col w-full">
+            <label htmlFor="input_adultos">Adultos</label>
+            <input id="input_adultos" type="number" className="p-2 border bg-white rounded" {...register("adultos")} min={1} max={9} step={1} />
+          </div>
+          <div className="flex flex-col w-full">
+            <label htmlFor="input_criancas">Crianças</label>
+            <input id="input_criancas" type="number" className="p-2 border bg-white rounded" {...register("criancas")} min={0} max={9} step={1} />
+          </div>
+          <div className="flex flex-col w-full">
+            <label htmlFor="input_bebes">Bebês</label>
+            <input id="input_bebes" type="number" className="p-2 border bg-white rounded" {...register("bebes")} min={0} max={9} step={1} />
+          </div>
+        </div>
+        <div className="flex gap-4 w-full">
+          <div className="flex flex-col w-full">
+            <label htmlFor="input_cabinCategory">Classe</label>
+            <select id="input_cabinCategory" className="p-2 border bg-white rounded" {...register("cabinCategory")}>
+              <option value="ECONOMY">Econômica</option>
+              <option value="BUSINESS">Executiva</option>
+            </select>
+          </div>
+          <label htmlFor="input_onlyDirect" className="w-full flex items-center">
+            <input id="input_onlyDirect" type="checkbox" className="mr-2" {...register("onlyDirect")} />
+            Somente voos diretos
+          </label>
+        </div>
+        <button type="submit" className="p-2 bg-blue-500 text-white rounded">Buscar</button>
+      </form>
+    </div>
   )
 }
---
+++ src/app/search/page.tsx
@@ -0,0 +1,130 @@
+import { NextPage } from "next"
+import makeFetchCookie from "fetch-cookie"
+
+interface SearchProps {
+  from: string
+  to: string
+  adult: string
+  child: string
+  infant: string
+  cabin: string
+  tripType: string
+  flightType: string
+  companies: string
+  derpatureDateTime: string
+  returnDateTime: string
+}
+
+const Page: NextPage<{ searchParams: SearchProps }> = async ({
+  searchParams: {
+    from,
+    to,
+    adult,
+    child,
+    infant,
+    cabin,
+    tripType,
+    flightType,
+    companies,
+    derpatureDateTime,
+    returnDateTime
+  }
+}) => {
+  const url = new URL("https://interline.tudoazul.com/catalog/api/v1/availability")
+  url.searchParams.set("tripType", tripType)
+  url.searchParams.set("origin", from)
+  url.searchParams.set("destination", to)
+  url.searchParams.set("adult", adult)
+  url.searchParams.set("child", child)
+  url.searchParams.set("infant", infant)
+  url.searchParams.set("cabinCategory", cabin)
+  url.searchParams.set("typeOfFlight", flightType)
+  url.searchParams.set("companiesIdentity", companies)
+  url.searchParams.set("departureDateTime", derpatureDateTime)
+  url.searchParams.set("returnDateTime", returnDateTime)
+
+  const cookieJar = new makeFetchCookie.toughCookie.CookieJar()
+
+  const fetchCookie = makeFetchCookie(fetch, cookieJar)
+
+  await fetchCookie("https://interline.tudoazul.com", {
+    headers: {
+      "user-agent":
+        "Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36"
+    }
+  })
+  const response = await fetchCookie(url.toString(), {
+    headers: {
+      "user-agent":
+        "Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36"
+    }
+  })
+  const data = await response.json()
+
+  let flights = data?.departureFlights?.flights as {
+    id: string
+    originAirport: string
+    originAirportName: string
+    finalDestination: string
+    finalDestinationName: string
+    operatingCarriers: string[]
+    prices: {
+      value: number
+      currency: string
+      classType: string
+    }[]
+  }[]
+
+  const returnFlights = await Promise.all(flights.map(async flight => {
+    const response = await fetchCookie("https://interline.tudoazul.com/exchange/api/v1/exchange/getPoints", {
+      method: "POST",
+      headers: {
+        "user-agent":
+          "Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36",
+        "content-type": "application/json;charset=UTF-8"
+      },
+      body: JSON.stringify({
+        items: {
+          origin: flight.originAirport,
+          destination: flight.finalDestination,
+          cabin,
+          classType: flight.prices[0].classType,
+          departureDate: derpatureDateTime,
+          partner: flight.operatingCarriers[0]
+        },
+        value: {
+          currency: flight.prices[0].currency,
+          value: flight.prices[0].value
+        }
+      })
+    })
+
+    const data = await response.json()
+
+    return {
+      ...flight,
+      points: data.points as number
+    }
+  }))
+
+  return (
+    <div>
+      {returnFlights.map(flight => (
+        <div key={flight.id}>
+          <div className="flex items-center justify-center gap-4">
+            <span>{flight.originAirportName}</span>
+            <span>→</span>
+            <span>{flight.finalDestinationName}</span>
+          </div>
+          <div className="flex items-center justify-center gap-4">
+            <span>{flight.prices[0].value}</span>
+            <span>{flight.points}</span>
+            <span>pontos</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Page
