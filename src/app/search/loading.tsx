300e1ad9820be9f65d314f778d67256b354e3ea0
Vitor Daniel <vitor036daniel@gmail.com>
2024-01-02 13:25:46 -0300

    fix

---
+++ src/app/search/loading.tsx
@@ -0,0 +1,11 @@
+export default () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center justify-center gap-4">
+        <div className="w-4 h-4 bg-gray-400 rounded-full animate-bounce"></div>
+        <div className="w-4 h-4 bg-gray-400 rounded-full animate-bounce"></div>
+        <div className="w-4 h-4 bg-gray-400 rounded-full animate-bounce"></div>
+      </div>
+    </div>
+  )
+}
